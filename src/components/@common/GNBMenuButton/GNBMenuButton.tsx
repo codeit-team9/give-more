d4b25498c663da30089727d9a33fc19f2990a33e
miniposi <rndtn6851@gmail.com>
2023-12-21 17:17:10 +0900

    fix: media queries로 수정

--- src/components/@common/GNBMenuButton/GNBMenuButton.tsx
+++ src/components/@common/GNBMenuButton/GNBMenuButton.tsx
@@ -1,20 +1,11 @@
 import styles from '@/components/@common/GNBMenuButton/GNBMenuButton.module.css';
 
 interface Props {
-  size: number;
   text: string;
 }
 
-function GNBMenuButton({ size, text }: Props) {
-  const wrapperStyles = {
-    fontSize: `${size}rem`,
-  };
-
-  return (
-    <div style={wrapperStyles} className={styles.wrapper}>
-      {text}
-    </div>
-  );
+function GNBMenuButton({ text }: Props) {
+  return <div className={styles.wrapper}>{text}</div>;
 }
 
 export default GNBMenuButton;
