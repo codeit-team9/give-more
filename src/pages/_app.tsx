08a7c917d0dce575813ab69d74662fcc34559ace
lsc58461 <lsc58461@naver.com>
2023-12-21 12:15:58 +0900

    feat: Imported colors.css in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/reset.css'
 import '@/styles/fonts.css'
+import '@/styles/colors.css'
 import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
