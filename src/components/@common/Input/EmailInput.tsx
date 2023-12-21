d4803f3d216e8e83f7319f8f01009086858d6c28
marchfirst01 <marchfirst3113@gmail.com>
2023-12-22 00:07:12 +0900

    feat: validation에 맞지 않을 경우 테두리 색 변경

--- src/components/@common/Input/EmailInput.tsx
+++ src/components/@common/Input/EmailInput.tsx
@@ -3,6 +3,7 @@ import styles from './Input.module.css';
 import Input from './Input';
 
 function EmailInput() {
+  const [isError, setIsError] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
 
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -10,14 +11,16 @@ function EmailInput() {
     const EmaliRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     if (!EmaliRegex.test(InputValue)) {
       setErrorMsg('잘못된 이메일입니다.');
+      setIsError(true);
     } else {
       setErrorMsg('');
+      setIsError(false);
     }
   };
 
   return (
     <>
-      <Input placeholder="입력" name="이메일" onChangeInput={onChangeInput} />
+      <Input name="이메일" isError={isError} onChangeInput={onChangeInput} />
       <p className={styles.errorMsg}>{errorMsg}</p>
     </>
   );
--- src/components/@common/Input/Input.tsx
+++ src/components/@common/Input/Input.tsx
@@ -1,17 +1,24 @@
+import classNames from 'classnames';
 import styles from './Input.module.css';
 
 interface Props {
   placeholder?: string;
   name: string;
+  isError: boolean;
   type?: string;
   onChangeInput?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function Input({ placeholder = '입력', name, type, onChangeInput }: Props) {
+function Input({ placeholder = '입력', name, isError, type, onChangeInput }: Props) {
   return (
     <div className={styles.wrapper}>
       <p>{name}</p>
-      <input className={styles.input} placeholder={placeholder} type={type} onChange={onChangeInput} />
+      <input
+        className={classNames(styles.input, isError ? styles.error : '')}
+        placeholder={placeholder}
+        type={type}
+        onChange={onChangeInput}
+      />
     </div>
   );
 }
