7c3d9bfb54d9d6ea2254747a2b6c0066852dfe2f
miniposi <rndtn6851@gmail.com>
2023-12-21 14:30:19 +0900

    feat: 로고 버튼 클릭 시 공고 리스트 페이지 이동

--- src/components/@common/LogoButton/LogoButton.tsx
+++ src/components/@common/LogoButton/LogoButton.tsx
@@ -1,19 +1,22 @@
-import LogoIcon from '@/assets/images/logo_icon.svg'
+import Link from 'next/link';
+import LogoIcon from '@/assets/images/logo_icon.svg';
 
-interface LogoButtonProps {
-  width: number
+interface Props {
+  width: number;
 }
 
-function LogoButton({ width }: LogoButtonProps) {
+function LogoButton({ width }: Props) {
   const wrapperStyles = {
     width: `${width}rem`,
-  }
+  };
 
   return (
     <div style={wrapperStyles}>
-      <LogoIcon />
+      <Link href="/jobLists">
+        <LogoIcon />
+      </Link>
     </div>
-  )
+  );
 }
 
-export default LogoButton
+export default LogoButton;
---
+++ src/pages/jobLists.tsx
@@ -0,0 +1,5 @@
+function JobListPage() {
+  return <>공고 리스트 페이지</>;
+}
+
+export default JobListPage;
