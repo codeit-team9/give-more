c6468d43de883a168ef8d0f98df6e77f5f371711
lsc58461 <lsc58461@naver.com>
2023-12-21 21:48:52 +0900

    feat: closed 상태 추가

--- src/components/notice/WageBadge/WageBadge.tsx
+++ src/components/notice/WageBadge/WageBadge.tsx
@@ -3,9 +3,10 @@ import WagebadgeUI from './WageBadgeUI';
 interface Props {
   defaultWage: number;
   currentWage: number;
+  isClosed: boolean;
 }
 
-function Wagebadge({ defaultWage, currentWage }: Props) {
+function Wagebadge({ defaultWage, currentWage, isClosed }: Props) {
   const averageWage = Math.trunc(((currentWage - defaultWage) / defaultWage) * 10) * 10;
 
   const determineArrowDirection = () => {
@@ -15,7 +16,7 @@ function Wagebadge({ defaultWage, currentWage }: Props) {
     return '↓';
   };
 
-  return <WagebadgeUI averageWage={averageWage} cursor={determineArrowDirection()} />;
+  return <WagebadgeUI averageWage={averageWage} cursor={determineArrowDirection()} isClosed={isClosed} />;
 }
 
 export default Wagebadge;
--- src/components/notice/WageBadge/WageBadgeUI.tsx
+++ src/components/notice/WageBadge/WageBadgeUI.tsx
@@ -4,13 +4,15 @@ import styles from './WageBadgeUI.module.css';
 interface Props {
   averageWage: number;
   cursor: '↑' | '↓';
+  isClosed: boolean;
 }
 
-function WagebadgeUI({ averageWage, cursor }: Props) {
+function WagebadgeUI({ averageWage, cursor, isClosed }: Props) {
   const dynamicColorClass = averageWage >= 50 ? styles.red40 : styles.red30;
+  const determinedClass = isClosed ? styles.closed : dynamicColorClass;
 
   return (
-    <div className={classNames(styles.wrapper, dynamicColorClass)}>
+    <div className={classNames(styles.wrapper, determinedClass)}>
       기존 시급보다 {averageWage}% {cursor}
     </div>
   );
