d861a6678b023bfce33d24a3cab14a8833c3c83a
lsc58461 <lsc58461@naver.com>
2023-12-21 21:51:50 +0900

    fix: font-size 62.5% 적용 안되는 문제 #28

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/reset.css'
-import '@/styles/fonts.css'
-import '@/styles/colors.css'
-import type { AppProps } from 'next/app'
+import '@/styles/reset.css';
+import '@/styles/fonts.css';
+import '@/styles/colors.css';
+import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
