1b962106d485a978f3fcd2edd867e1cd9e386e63
miniposi <rndtn6851@gmail.com>
2023-12-21 18:02:42 +0900

    fix: media queries 추가

--- src/components/@common/SearchBar/SearchBar.tsx
+++ src/components/@common/SearchBar/SearchBar.tsx
@@ -1,56 +1,11 @@
 import SearchIcon from '@/assets/images/search_icon.svg';
 import styles from '@/components/@common/SearchBar/SearchBar.module.css';
 
-interface Props {
-  size: keyof Size;
-}
-
-interface DeviceSize {
-  width: number;
-  gap: number;
-  fontSize: number;
-  iconSize: number;
-}
-
-interface Size {
-  desktop: DeviceSize;
-  tablet: DeviceSize;
-  mobile: DeviceSize;
-}
-
-const Device: Size = {
-  desktop: {
-    width: 28.125,
-    gap: 0.625,
-    fontSize: 0.875,
-    iconSize: 1.25,
-  },
-  tablet: {
-    width: 21.5,
-    gap: 0.625,
-    fontSize: 0.875,
-    iconSize: 1.25,
-  },
-  mobile: {
-    width: 20.9375,
-    gap: 0.5,
-    fontSize: 0.75,
-    iconSize: 1,
-  },
-};
-
-function SearchBar({ size }: Props) {
-  const wrapperStyles = {
-    width: `${Device[size].width}rem`,
-    gap: `${Device[size].gap}rem`,
-    padding: `${Device[size].gap}rem`,
-    fontSize: `${Device[size].fontSize}rem`,
-  };
-
+function SearchBar() {
   return (
-    <div style={wrapperStyles} className={styles.wrapper}>
-      <SearchIcon style={{ width: `${Device[size].iconSize}rem` }} />
-      <p>가게 이름으로 찾아보세요</p>
+    <div className={styles.wrapper}>
+      <SearchIcon className={styles.searchIcon} />
+      <input type="text" placeholder="가게 이름으로 찾아보세요" />
     </div>
   );
 }
