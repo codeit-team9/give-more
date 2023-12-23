0e4947af5c77efd3d1f6b3edf412b7089139930d
marchfirst01 <marchfirst3113@gmail.com>
2023-12-23 10:45:54 +0900

    feat: HourlyPayInput 이름 변경

--- src/components/@common/Input/HourlyPayInput.tsx
+++ src/components/@common/Input/HourlyPayInput.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import Input from './Input';
 
-function WageInput() {
+function HourlyPayInput() {
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.target.value = e.target.value.replace(/\D/g, '').replace(/([0-9]{1,3})([0-9]{3})/g, '$1,$2');
   };
@@ -9,4 +8,4 @@ function WageInput() {
   return <Input name="시급*" maxLength={7} innerText="원" onChangeInput={onChangeInput} />;
 }
 
-export default WageInput;
+export default HourlyPayInput;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import HourlyPayInput from '@/components/@common/Input/HourlyPayInput';
+
 function Home() {
-  return <>hello world!</>;
+  return (
+    <>
+      hello world!
+      <HourlyPayInput />
+    </>
+  );
 }
 
 export default Home;
