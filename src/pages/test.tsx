c705c30838e8ea65887ef4dccf8a1d8f5f3d7031
임석준 <sjlim@SJLIM-MacBook-Pro.local>
2023-12-26 15:45:32 +0900

    PreferredAreaDropdown 초기 틀 구성

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import PreferredAreaDropdown from '@/components/@common/DropDown/PreferredAreaDropdown';
+
+function test() {
+  return (
+    <>
+      공고 리스트 페이지
+      <PreferredAreaDropdown />
+    </>
+  );
+}
+
+export default test;
