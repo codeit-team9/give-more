6df58521f5bc072b85333e630d091eea75874e7d
miniposi <rndtn6851@gmail.com>
2023-12-28 21:52:23 +0900

    feat: 공통 버튼

---
+++ src/components/@common/Button/NotSolidButton.tsx
@@ -0,0 +1,7 @@
+import styles from './NotSolidButton.module.css';
+
+function NotSolidButton() {
+	return <div className={styles.wrapper}>로그인하기</div>;
+}
+
+export default NotSolidButton;
---
+++ src/components/@common/Button/SolidButton.tsx
@@ -0,0 +1,7 @@
+import styles from './SolidButton.module.css';
+
+function SolidButton() {
+	return <div className={styles.wrapper}>로그인하기</div>;
+}
+
+export default SolidButton;
