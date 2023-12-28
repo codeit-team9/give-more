55f47dc7b49378b9dc346df29009e28909469974
miniposi <rndtn6851@gmail.com>
2023-12-28 23:39:47 +0900

    refactor: login button 합치기

--- src/components/@common/Button/NotSolidButton.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './NotSolidButton.module.css';
-
-function NotSolidButton() {
-	return <div className={styles.wrapper}>로그인하기</div>;
-}
-
-export default NotSolidButton;
--- src/components/@common/Button/SolidButton.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './SolidButton.module.css';
-
-function SolidButton() {
-	return <div className={styles.wrapper}>로그인하기</div>;
-}
-
-export default SolidButton;
