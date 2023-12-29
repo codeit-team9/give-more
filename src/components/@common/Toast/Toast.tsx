9fc2adf178f675e94deff51a7b6b56078da0e994
miniposi <rndtn6851@gmail.com>
2023-12-29 16:43:52 +0900

    feat: Toast 구현

---
+++ src/components/@common/Toast/Toast.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { TOAST_MESSAGE } from '@/constants/TOAST';
+import styles from './Toast.module.css';
+
+interface Props {
+  type: string;
+}
+
+function Toast({ type }: Props) {
+  const [isActive, setIsActive] = useState(false);
+  const content = TOAST_MESSAGE[type].message;
+
+  useEffect(() => {
+    setIsActive(true);
+    const timer = setTimeout(() => {
+      setIsActive(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return isActive ? <div className={styles.wrapper}>{content}</div> : null;
+}
+
+export default Toast;
