ede8f44212b774cb704c68be191d51e5158b3c41
lsc58461 <lsc58461@naver.com>
2023-12-30 00:18:37 +0900

    style: 코드 스타일 수정

--- src/components/@common/Button/LoginButton.tsx
+++ src/components/@common/Button/LoginButton.tsx
@@ -1,15 +1,15 @@
-import styles from './LoginButton.module.css';
 import classnames from 'classnames';
+import styles from './LoginButton.module.css';
 
 interface Props {
-	type: 'solid' | 'notsolid';
+  type: 'solid' | 'notSolid';
 }
 
-function LoginButton({type}: Props) {
-	const isSolid = type === 'solid';
-	const buttonClassName = isSolid ? styles.solid : styles.notsolid;
+function LoginButton({ type }: Props) {
+  const isSolid = type === 'solid';
+  const buttonClassName = isSolid ? styles.solid : styles.notSolid;
 
-	return	<div className={classnames(styles.button, buttonClassName)}>로그인하기</div>;
+  return <div className={classnames(styles.button, buttonClassName)}>로그인하기</div>;
 }
 
 export default LoginButton;
