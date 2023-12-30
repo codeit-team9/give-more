c18d19197e71eedff69816091c94f6b46132d1de
SJvaca30 <yyvaca30@gmail.com>
2023-12-30 19:38:18 +0900

    fix: test.tsx 삭제

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,15 +1,5 @@
-import Dropdown from '@/components/@common/DropDown/Dropdown';
-import { CATEGORY, LOCATIONS, SORT } from '@/constants/constants';
-
 function test() {
-  return (
-    <>
-      <Dropdown title="선호 지역" item={LOCATIONS} />
-      <Dropdown title="주소" item={LOCATIONS} />
-      <Dropdown title="분류" item={CATEGORY} />
-      <Dropdown type="sort" item={SORT} />
-    </>
-  );
+  return <>test</>;
 }
 
 export default test;
