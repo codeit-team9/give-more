d169de95c48f51cca93cd27a9feed792d5086a0c
miniposi <rndtn6851@gmail.com>
2023-12-30 19:32:48 +0900

    feat: 로그인, 로그아웃 임시 페이지

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import LogoButton from '@/components/@common/LogoButton/LogoButton';
+import styles from './signup.module.css';
+import EmailInput from '@/components/@common/Input/EmailInput';
+import PasswordInput from '@/components/@common/Input/PasswordInput';
+import PrimaryButton from '@/components/@common/Button/PrimaryButton';
+
+function SignUp() {
+  return (
+    <div className={styles.wrapper}>
+      <LogoButton isSign />
+      <div className={styles.input}>
+        <EmailInput />
+        <PasswordInput />
+        <PrimaryButton text="로그인하기" />
+      </div>
+      <div>
+        회원이 아니신가요? &nbsp; &nbsp;
+        <Link href="/" className={styles.link}>
+          회원가입하기
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
