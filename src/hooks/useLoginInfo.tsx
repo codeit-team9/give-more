7b303cd7ed5e6e77ca29320f27c8a78c97e971e8
miniposi <rndtn6851@gmail.com>
2023-12-30 14:30:00 +0900

    feat: access token 추가

--- src/hooks/useLoginInfo.tsx
+++ src/hooks/useLoginInfo.tsx
@@ -3,11 +3,15 @@ import { create } from 'zustand';
 interface LoginInfoState {
   isLogin: boolean;
   setIsLogin: (isLogin: boolean) => void;
+  token: string;
+  setToken: (token: string) => void;
 }
 
 const useLoginInfo = create<LoginInfoState>((set) => ({
   isLogin: false,
   setIsLogin: (isLogin) => set({ isLogin }),
+  token: '',
+  setToken: (token) => set({ token }),
 }));
 
 export default useLoginInfo;
