16de2d736ed2ecf97085669515eeba952a8acb56
miniposi <rndtn6851@gmail.com>
2023-12-31 21:01:05 +0900

    fix: 문법 오류 해결

--- src/components/@common/UserTypeButton/UserTypeButton.tsx
+++ src/components/@common/UserTypeButton/UserTypeButton.tsx
@@ -1,23 +1,46 @@
-
 import { useState } from 'react';
+import classNames from 'classnames';
 import CheckedIcon from './CheckedIcon';
 import Blanked from '@/assets/images/blanked_icon.svg';
 import styles from './UserTypeButton.module.css';
-import classNames from 'classnames';
+import useSignup from '@/hooks/useSignup';
 
 function UserTypeButton() {
-	const [isSelected, setIsSelected] = useState(true);
+  const [isSelected, setIsSelected] = useState(true);
+  const { setIsUser } = useSignup();
 
-	const handleSelected = () => {
-		setIsSelected(!isSelected);
-	};
+  const handleSelected = () => {
+    setIsSelected(!isSelected);
+    if (isSelected) {
+      setIsUser('employee');
+    } else {
+      setIsUser('employer');
+    }
+  };
 
-	return (
-		<div className={styles.wrapper}>
-			<button className={classNames(styles.button, isSelected ? styles.selected : styles.notSelected)} onClick={() => handleSelected()}>{isSelected ? <CheckedIcon/> : <Blanked/>}<span>알바님</span></button>
-			<button className={classNames(styles.button, isSelected ? styles.notSelected : styles.selected)} onClick={() => handleSelected()}>{isSelected ? <Blanked/> : <CheckedIcon/>}<span>사장님</span></button>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      회원 유형
+      <div className={styles.wrapper}>
+        <button
+          type="button"
+          className={classNames(styles.button, isSelected ? styles.selected : styles.notSelected)}
+          onClick={() => handleSelected()}
+        >
+          {isSelected ? <CheckedIcon /> : <Blanked />}
+          <span>알바님</span>
+        </button>
+        <button
+          type="button"
+          className={classNames(styles.button, isSelected ? styles.notSelected : styles.selected)}
+          onClick={() => handleSelected()}
+        >
+          {isSelected ? <Blanked /> : <CheckedIcon />}
+          <span>사장님</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default UserTypeButton;
