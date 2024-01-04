84d9d8d017ec4e7ff458c3722fc92960ba24e323
lsc58461 <lsc58461@naver.com>
2024-01-04 16:28:47 +0900

    refactor: 수정

--- src/components/notice/NoticeList/NoticeList.tsx
+++ src/components/notice/NoticeList/NoticeList.tsx
@@ -1,8 +1,9 @@
 import classNames from 'classnames';
 import NoticeCard from '../NoticeCard/NoticeCard';
-import styles from './NoticeList.module.css';
 import formatWorkTime from '@/utils/formatWorkTime';
 import { Notice } from '@/@types/notice.types';
+import styles from './NoticeList.module.css';
+import NotFoundNotice from '../NotFoundNotice/NotFoundNotice';
 
 interface Props {
   type: 'customized' | 'entire';
@@ -13,27 +14,30 @@ interface Props {
 function NoticeList({ type, items, count }: Props) {
   return (
     <ul className={classNames(styles.wrapper, styles[type])}>
-      {items?.slice(0, count).map((notice: Notice) => {
-        const { id, hourlyPay, startsAt, workhour, closed, shop } = notice.item;
-        const { name, address1, imageUrl, originalHourlyPay } = shop.item;
+      {items && items?.length > 0 ? (
+        items.slice(0, count).map((notice: Notice) => {
+          const { id, hourlyPay, startsAt, workhour, closed, shop } = notice.item;
+          const { name, address1, imageUrl, originalHourlyPay } = shop.item;
 
-        const formattedWorkTime = formatWorkTime({ type: 'notice', startsAt, workHour: workhour });
+          const formattedWorkTime = formatWorkTime({ type: 'notice', startsAt, workHour: workhour });
 
-        // eslint-disable-next-line consistent-return
-        return (
-          <li key={id}>
-            <NoticeCard
-              cardImageUrl={imageUrl}
-              restaurantName={name}
-              duration={formattedWorkTime}
-              address={address1}
-              defaultHourlyPay={originalHourlyPay}
-              currentHourlyPay={hourlyPay}
-              isClosed={closed}
-            />
-          </li>
-        );
-      })}
+          return (
+            <li key={id}>
+              <NoticeCard
+                cardImageUrl={imageUrl}
+                restaurantName={name}
+                duration={formattedWorkTime}
+                address={address1}
+                defaultHourlyPay={originalHourlyPay}
+                currentHourlyPay={hourlyPay}
+                isClosed={closed}
+              />
+            </li>
+          );
+        })
+      ) : (
+        <NotFoundNotice />
+      )}
     </ul>
   );
 }
