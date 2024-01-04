9c28f62e8cfbe239334a65039a3ad86918025032
miniposi <rndtn6851@gmail.com>
2024-01-04 18:48:23 +0900

    feat: 사용자 정보 등록

---
+++ src/hooks/useUserInfo.tsx
@@ -0,0 +1,43 @@
+import { create } from 'zustand';
+
+interface UserInfoState {
+  id: string;
+  setId: (id: string) => void;
+  email: string;
+  setEmail: (email: string) => void;
+  type: 'employee' | 'employer';
+  setType: (UserType: 'employee' | 'employer') => void;
+  name: string;
+  setName: (name: string) => void;
+  phone: string;
+  setPhone: (phone: string) => void;
+  address: string;
+  setAddress: (address: string) => void;
+  bio: string;
+  setBio: (bio: string) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  shop: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  setShop: (shop: any) => void;
+}
+
+const useUserInfo = create<UserInfoState>((set) => ({
+  id: '',
+  setId: (id) => set({ id }),
+  email: '',
+  setEmail: (email) => set({ email }),
+  type: 'employee',
+  setType: (type) => set({ type }),
+  name: '',
+  setName: (name) => set({ name }),
+  phone: '',
+  setPhone: (phone) => set({ phone }),
+  address: '',
+  setAddress: (address) => set({ address }),
+  bio: '',
+  setBio: (bio) => set({ bio }),
+  shop: null,
+  setShop: (shop) => set({ shop }),
+}));
+
+export default useUserInfo;
